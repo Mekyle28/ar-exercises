b1cf67a17bfebeff48dd7b6bb9de7f8486297e3e
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-25 17:09:56 -0400

    output total revenue, average revenue and number of stores with revenue over 1m

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum("annual_revenue")
+puts "the total annual revenue for all the stores is #{@total_revenue}"
+
+@average_revenue = Store.average("annual_revenue")
+puts "the average annual store revenue is #{@average_revenue}"
+
+@stores_1m_plus = Store.where(annual_revenue: 1000000...).count
+puts @stores_1m_plus
