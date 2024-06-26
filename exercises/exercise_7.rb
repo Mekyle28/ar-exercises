a82279ede5ab8c61ac6a07ebadfd635bf2a87ac2
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-25 20:39:17 -0400

    test the validation by trying to create an incomplete db input and render the error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please input a store name"
+@user_store_name = gets.chomp
+
+
+@store7 = Store.new(name: @user_store_name)
+
+if @store7.save
+  puts "The store was added to the database"
+else
+  puts "Failed to save due to errors: "
+  @store7.errors.full_messages.each do |error_message|
+    puts "- #{error_message}"
+  end
+end
+
+
